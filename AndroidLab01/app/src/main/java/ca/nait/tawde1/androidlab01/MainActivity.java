6d5c4185809ead00451ecb0957d1412e9f7578f5
Taylor <35233492+tajunk@users.noreply.github.com>
2020-02-06 16:32:04 -0700

    update AndroidLab01

--- AndroidLab01/app/src/main/java/ca/nait/tawde1/androidlab01/MainActivity.java
+++ AndroidLab01/app/src/main/java/ca/nait/tawde1/androidlab01/MainActivity.java
@@ -1,41 +1,90 @@
 package ca.nait.tawde1.androidlab01;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener
 {
+    SharedPreferences prefs;
+    View mainView;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(Build.VERSION.SDK_INT > 9)
+        {
+            StrictMode.ThreadPolicy ourPolicy =
+                    new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(ourPolicy);
+        }
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        prefs.registerOnSharedPreferenceChangeListener(this);
+
+        mainView = findViewById(R.id.main_page_layout);
+        String bgColor = prefs.getString(getResources().getString(R.string.preference_key_main_bg_color), "#FCF8CF");
+        mainView.setBackgroundColor(Color.parseColor(bgColor));
+
     }
 
     public void onRadioButtonClicked(View view)
     {
-        boolean checked = ((RadioButton) view).isChecked();
+        RadioGroup rg = (RadioGroup)findViewById(R.id.radio_group_categories);
+        int rBid = rg.getCheckedRadioButtonId();
+        RadioButton rb = (RadioButton)findViewById(rBid);
+        String category = (String)rb.getTag();
+    }
 
-        switch(view.getId())
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = this.getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        switch(item.getItemId())
         {
-            case R.id.radio_category_picture:
-                if (checked)
-                    break;
-            case R.id.radio_category_actor:
-                if (checked)
-                    break;
-            case R.id.radio_category_actress:
-                if (checked)
-                    break;
-            case R.id.radio_category_editing:
-                if (checked)
-                    break;
-            case R.id.radio_category_effects:
-                if (checked)
-                    break;
+            case R.id.menu_item_home:
+            {
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.menu_item_preferences:
+            {
+                Intent intent = new Intent(this, PrefsActivity.class);
+                startActivity(intent);
+                break;
+            }
+
         }
+        return true;
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key)
+    {
+        String bgColor = prefs.getString(getResources().getString(R.string.preference_key_main_bg_color), "#990000");
+        mainView.setBackgroundColor(Color.parseColor(bgColor));
     }
 }
